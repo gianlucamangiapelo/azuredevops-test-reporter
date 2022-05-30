13aee4a9e02040acb23e1e51e4236ac1a72890e3
Gianluca <gianluca.mangiapelo@gmail.com>
2022-05-30 17:08:08 +0200

    feat: update getPoints() and relative tests

--- src/services/azure/testCasePoints.ts
+++ src/services/azure/testCasePoints.ts
@@ -5,21 +5,31 @@ export async function getPoints(
   axiosClient: AxiosInstance,
   config: IAzureConfig
 ): Promise<number[]> {
-  if (!axiosClient) {
+  if (!axiosClient || !Object.keys(axiosClient).length) {
     return new Promise(() => {
       throw new Error('Missing valid Azure Devops client')
     })
   }
 
-  const testCasesPoints = await axiosClient.get(
-    `/testplan/Plans/${config.planId}/Suites/${config.suiteId}/TestCase?witFields=System.Id&excludeFlags=0&isRecursive=true`
-  )
+  const testCasesPoints = await axiosClient
+    .get(
+      `/testplan/Plans/${config.planId}/Suites/${config.suiteId}/TestCase?witFields=System.Id&excludeFlags=0&isRecursive=true`
+    )
+    .catch((error) => {
+      throw new Error(error)
+    })
 
-  const testCasePointIds: number[] = testCasesPoints.data.value.map(
-    (val: { pointAssignments: { id: number }[] }) => {
-      return val.pointAssignments[0].id
-    }
-  )
+  const testCasePointIds: number[] = testCasesPoints.data.value
+    .filter((val: { pointAssignments: { id: number }[] }) => {
+      if (val.pointAssignments) {
+        return true
+      }
+      return false
+    })
+    .map(
+      (val: { pointAssignments: { id: number }[] }) =>
+        val.pointAssignments[0]?.id
+    )
 
   return testCasePointIds
 }
