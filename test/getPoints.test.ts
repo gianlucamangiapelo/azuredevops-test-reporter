f1d142ff212b0a037e1da0618914b7ae2ef8e174
Gianluca <gianluca.mangiapelo@gmail.com>
2022-05-30 17:08:08 +0200

    feat: update getPoints() and relative tests

--- test/getPoints.test.ts
+++ test/getPoints.test.ts
@@ -1,21 +1,8 @@
+import { AxiosInstance } from 'axios'
 import { ITestApi } from 'azure-devops-node-api/TestApi'
 import { IAzureConfig } from '../src/interfaces/IAzureConfig'
-import {getPoints} from '../src/services/azure/testCasePoints'
+import { getPoints } from '../src/services/azure/testCasePoints'
 
-const mockClient = {
-  getPoints: jest.fn(()=>{
-    return new Promise((resolve, reject)=> {
-      resolve([{
-        configuration: {},
-        id: 1,
-        outcome: "0",
-        testCase: {},
-        url: "",
-        workItemProperties: []
-      }])
-    })
-  })
-}
 
 const azureConfig: IAzureConfig = {
   pat: 'v3e3pg4njfmzzcuoxwb4vja3trwbyafim4x4oww7sbcfbopfb3bq',
@@ -26,15 +13,69 @@ const azureConfig: IAzureConfig = {
   runName: 'sample',
 }
 
-
-describe('Validate http call to retrieve the testCase in the test Run', ()=>{
-  it.skip('Should resolve the promise if a valid AzureDevOps client is passed', async ()=>{
-    // const result =  getPoints(mockClient as unknown as ITestApi, azureConfig)
-    // await expect(result).resolves.not.toEqual([])
+describe('Validate http call to retrieve the testCase in the test Run', () => {
+  it('Should resolve an empty the promise if a valid AzureDevOps Axios client is passed', async () => {
+    const mockClient = {
+      get: jest.fn(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            data: {
+              value: [{
+                configuration: {},
+                id: 1,
+                outcome: "0",
+                testCase: {},
+                url: "",
+                workItemProperties: []
+              }]
+            }
+          })
+        })
+      })
+    }
+    const result = getPoints(mockClient as unknown as AxiosInstance, azureConfig)
+    await expect(result).resolves.toEqual([])
   })
 
-  it.skip('Should throw error an error if an empty AzureDevOps client is passed', async ()=>{
-    // const result = getPoints({} as ITestApi, azureConfig)
-    // await expect(result).rejects.toThrow();
+  it('Should return ids if data are wellformed from AzureDevOps API', async () => {
+    const mockClient = {
+      get: jest.fn(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            data: {
+              value: [{
+                pointAssignments: [{ id: 1 }, { id: 2 }],
+                configuration: {},
+                id: 1,
+              },
+              {
+                pointAssignments: [{ id: 3 }, { id: 4 }],
+                configuration: {},
+                id: 2,
+              }]
+            }
+          })
+        })
+      })
+    }
+    const result = getPoints(mockClient as unknown as AxiosInstance, azureConfig)
+    await expect(result).resolves.toEqual([1, 3])
+  })
+
+  it('Should throw error an error if an empty AzureDevOps Axios client is passed', async () => {
+    const result = getPoints({} as AxiosInstance, azureConfig)
+    await expect(result).rejects.toThrow('Missing valid Azure Devops client');
+  })
+
+  it('Should throw error if AzureDevOps Axios client is throwing an exception', async () => {
+    const mockClient = {
+      get: jest.fn(()=>{
+        return new Promise((resolve, reject)=>{
+          reject('rejected call');
+        })
+      })
+    }
+    const result = getPoints(mockClient as unknown as AxiosInstance, azureConfig)
+    await expect(result).rejects.toThrow('rejected call');
   })
 })
