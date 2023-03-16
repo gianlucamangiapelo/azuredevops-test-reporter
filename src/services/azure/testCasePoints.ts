4d171d92d012debb1b409edb40b75a21bcb689ac
Gianluca <gianluca.mangiapelo@deltatre.com>
2023-03-16 16:22:05 +0100

    feat: add continuation token to get testpoints

--- src/services/azure/testCasePoints.ts
+++ src/services/azure/testCasePoints.ts
@@ -11,25 +11,34 @@ export async function getPoints(
     })
   }
 
-  const testCasesPoints = await axiosClient
-    .get(
-      `/testplan/Plans/${config.planId}/Suites/${config.suiteId}/TestCase?witFields=System.Id&excludeFlags=0&isRecursive=true`
-    )
-    .catch((error) => {
-      throw new Error(error)
-    })
+  let continuationToken = ''
+  let fullTestCasePointIds: number[] = []
 
-  const testCasePointIds: number[] = testCasesPoints.data.value
-    .filter((val: { pointAssignments: { id: number }[] }) => {
-      if (val.pointAssignments) {
-        return true
-      }
-      return false
-    })
-    .map(
-      (val: { pointAssignments: { id: number }[] }) =>
-        val.pointAssignments[0]?.id
-    )
+  do {
+    const testCasesPoints = await axiosClient
+      .get(
+        `/testplan/Plans/${config.planId}/Suites/${config.suiteId}/TestCase?witFields=System.Id&continuationToken=${continuationToken}&excludeFlags=0&isRecursive=true`
+      )
+      .catch((error) => {
+        throw new Error(error)
+      })
 
-  return testCasePointIds
+    const testCasePointIds: number[] = testCasesPoints.data.value
+      .filter((val: { pointAssignments: { id: number }[] }) => {
+        if (val.pointAssignments) {
+          return true
+        }
+        return false
+      })
+      .map(
+        (val: { pointAssignments: { id: number }[] }) =>
+          val.pointAssignments[0]?.id
+      )
+
+    fullTestCasePointIds = [...fullTestCasePointIds, ...testCasePointIds]
+
+    continuationToken = testCasesPoints.headers['x-ms-continuationtoken']
+  } while (continuationToken !== undefined)
+
+  return fullTestCasePointIds
 }
